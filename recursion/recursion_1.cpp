32fd509ba6f85c98267e4b78bb758b04e1298919
Gaurang-Gaur <gauranggaur007@gmail.com>
2023-03-09 18:33:52 +0530

    adding the recursion

---
+++ recursion/recursion_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+
+    if (n == 1 || n==0)
+    {
+        return 1;
+    }
+    else
+    {
+
+        n = n* fact(n - 1);
+
+        return n;
+    }
+}
+
+int main()
+{
+    int n=0;
+    cout << "enter the value of n:" << endl;
+
+    cin >> n;
+
+    int ans=0;
+    ans = fact(n);
+
+    cout << ans << endl;
+
+    return 0;
+}
